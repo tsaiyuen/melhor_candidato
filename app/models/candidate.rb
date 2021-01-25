400da4882ad67c3aa14bb9bc6fea95cbf1301bd8
Cynthia Tsai <tsaiyuen@gmail.com>
2021-01-25 15:13:02 -0300

    created model, route and controller for candidates

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,2 +1,6 @@
 class Candidate < ApplicationRecord
+  validates :user, presence:true
+  validates :city, presence:true
+  validates :experience, presence:true
+  validates :technologies, presence:true
 end
