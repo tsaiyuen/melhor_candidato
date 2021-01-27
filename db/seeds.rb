f90b3e558d7f458ec47c7090476a63b91ce42274
Cynthia Tsai <tsaiyuen@gmail.com>
2021-01-27 13:28:55 -0300

    JSON filter on going. Need to solve Technology + main tech

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,36 @@
-puts "cleaning db"
-Candidate.destroy_all
-puts "Finished cleaning db"
+require 'json'
+require 'open-uri'
+require 'pry-byebug'
+url = URI("https://geekhunter-recruiting.s3.amazonaws.com/code_challenge.json")
 
-puts "Creating users"
-Candidate.create(user:1234, city:"Rio de Janeiro - RJ", experience:"1-2", technologies:"Python, Ruby, Pearl")
-Candidate.create(user:5678, city:"Belo Horizonte - MG", experience:"0-1", technologies:"JavaScript")
-puts "Finished Creating users"
+file = open(url).read
+db = JSON.parse(file)
+
+
+technologies = []
+main = []
+db["candidates"].each do |info|
+  user = info["id"]
+  city = info["city"]
+  experience = info["experience"]
+  info["technologies"].each do |n|
+    name = n["name"]
+    main_tech = n["is_main_tech"]
+    technologies << name 
+    main << main_tech
+    end 
+  binding.pry
+end
+
+
+
+
+
+#puts "cleaning db"
+#Candidate.destroy_all
+#puts "Finished cleaning db"
+#
+#puts "Creating users"
+#Candidate.create(user:1234, city:"Rio de Janeiro - RJ", experience:"1-2", technologies:"Python, Ruby, Pearl")
+#Candidate.create(user:5678, city:"Belo Horizonte - MG", experience:"0-1", technologies:"JavaScript")
+#puts "Finished Creating users"
