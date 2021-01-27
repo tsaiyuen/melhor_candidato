d11ef58417261cd9fe616b475ac5774aa4c817e8
Cynthia Tsai <tsaiyuen@gmail.com>
2021-01-27 17:29:23 -0300

    Still on Tech and main tech issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,35 @@
 require 'json'
 require 'open-uri'
 require 'pry-byebug'
+
+puts "cleaning db"
+Candidate.destroy_all
+puts "Finished cleaning db"
+
 url = URI("https://geekhunter-recruiting.s3.amazonaws.com/code_challenge.json")
 
 file = open(url).read
 db = JSON.parse(file)
 
 
-technologies = []
 main = []
 db["candidates"].each do |info|
+  technologies = []
   user = info["id"]
   city = info["city"]
   experience = info["experience"]
   info["technologies"].each do |n|
-    name = n["name"]
-    main_tech = n["is_main_tech"]
-    technologies << name 
-    main << main_tech
-    end 
-  binding.pry
+    technologies << n["name"]
+    main << n["is_main_tech"]
+  end 
+  Candidate.create(user: user, city: city, experience: experience, technologies: technologies)
 end
 
 
 
 
 
-#puts "cleaning db"
-#Candidate.destroy_all
-#puts "Finished cleaning db"
-#
+
 #puts "Creating users"
 #Candidate.create(user:1234, city:"Rio de Janeiro - RJ", experience:"1-2", technologies:"Python, Ruby, Pearl")
 #Candidate.create(user:5678, city:"Belo Horizonte - MG", experience:"0-1", technologies:"JavaScript")
